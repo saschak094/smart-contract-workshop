ef75c75e2cd6b85c9b5ab6ceeb27284d0b5797f5
Sascha <sascha.kerbler@accenture.com>
2018-04-05 10:01:13 +0200

    Remove EndCampaign transaction

--- crowdfunding.js
+++ crowdfunding.js
@@ -1,10 +1,10 @@
 /*
- * @param {sk.openslava.crowdfunding.FundCampaign} funding - funding to process
+ * @param {mountain.camp.crowdfunding.FundCampaign} funding - funding to process
  * @transaction
  */
 function processFunding(funding) {
     validateFunding(funding);
-    return getAssetRegistry('sk.openslava.crowdfunding.Project')
+    return getAssetRegistry('mountain.camp.crowdfunding.Campaign')
         .then(function (accountRegistry) {
             return accountRegistry.update(
                 incrementPledgeAmount(funding)
@@ -13,28 +13,27 @@ function processFunding(funding) {
 }
 
 /*
- * @param {sk.openslava.crowdfunding.FundProject} funding - funding to process
+ * @param {mountain.camp.crowdfunding.FundCampaign} funding - funding to process
  */
 function validateFunding(funding) {
     if (funding.amount <= 0) {
         throw new Error('Amount must be a positive value');
     }
-    if (funding.funder.balance < funding.amount ) {
+    if (funding.funder.balance < funding.amount) {
         throw new Error('Insufficient funds to perform the funding');
-    } 
-    if (funding.project.status === "CLOSED"){
+    }
+    if (funding.campaign.status === "CLOSED") {
         throw new Error('Funding period is already over');
     }
 }
 
-
 /*
- * @param {sk.openslava.crowdfunding.FundProject} funding - funding to process
+ * @param {mountain.camp.crowdfunding.FundCampaign} funding - funding to process
  */
 function incrementPledgeAmount(funding) {
     var amount = funding.amount;
-    var project = funding.project;
-    project.pledgeAmount = project.pledgeAmount + amount;
-    return project;
+    var campaign = funding.campaign;
+    campaign.pledgeAmount = campaign.pledgeAmount + amount;
+    return campaign;
 }
 
